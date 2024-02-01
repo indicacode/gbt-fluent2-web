5988b086635f18c289e2ad43fb3efc4a7e9b62ec
Willien <willien_dev@outlook.com>
2024-02-01 15:36:30 -0300

    Add all components from chadcn :)

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import './globals.css'
+
+
+export const metadata = {
+    title: 'MailPage',
+    description: 'Shadcn like mailpage',
+}
+
+export default function RootLayout({children}) {
+    return (
+        <html>
+        <body>{children}</body>
+        </html>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from '@/components/ui/button'
+
+export default function Home() {
+    return (
+        <>
+            <h1>
+                HELLO WORLD
+            </h1>
+            <Button variant="outline">Button</Button>
+        </>
+    )
+}
+
