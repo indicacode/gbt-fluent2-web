acefecd5014c5f1914f1d2e4650eb3e66fcadf08
Willien <willien_dev@outlook.com>
2024-02-06 09:15:39 -0300

    feat[gbt-fluent2-web]ReAdd input for testing

--- components/reviewing/input.tsx
+++ components/reviewing/input.tsx
@@ -1,41 +1,45 @@
 import * as React from "react"
 
 import {cn} from "@/lib/utils"
+import {tv} from "tailwind-variants";
 
-// Where variants are used here: https://fluent2.microsoft.design/components/web/react/input/usage ?
-// const inputVariants = cva(
-//   "flex  w-full rounded-md  bg-transparent px-4 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
-//   {
-//     variants: {
-//       variant: {
-//         default:
-//           "flex  w-full   bg-transparent px-2 border-t-2 border-l-2 border-r-2 border-b-2 focus:border-b-black focus:border-b-[1px] hover:border-b-[1px] hover:border-b-black text-sm shadow-sm transition-colors ",
-//       },
-//     },
-//     defaultVariants: {
-//       variant: "default",
-//     },
-//   }
-// )
+const inputVariants = tv(
+  {
+  base:"flex  w-full rounded-md  bg-transparent px-4 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+    variants: {
+      variant: {
+        default:
+          "flex  w-full   bg-transparent px-2 border-t-2 border-l-2 border-r-2 border-b-2 focus:border-b-black focus:border-b-[1px] hover:border-b-[1px] hover:border-b-black text-sm shadow-sm transition-colors ",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
 export interface InputProps
     extends React.InputHTMLAttributes<HTMLInputElement> {
     className?: string
+    placeholderText:string
+    placeholderClasses:string
+    subtextClasses:string
+    subtext:string
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({className, ...props}, ref) => {
+    ({className,placeholderText,placeholderClasses,subtextClasses,subtext, ...props}, ref) => {
         return (
-            // <div className={"flex   flex-col"}>
-            //   <h2 className={" text-black  py-1" + placeholderClasses}>
-            //     {placeholderText}
-            //   </h2>
+            <div className={"flex   flex-col"}>
+              <h2 className={" text-black  py-1" + placeholderClasses}>
+                {placeholderText}
+              </h2>
             <input
                 className={cn(className + " placeholder-slate-400")}
                 ref={ref}
                 {...props}
             />
-            //   <h2 className={"" + subtextClasses}>{subtext}</h2>
-            // </div>
+               <h2 className={"" + subtextClasses}>{subtext}</h2>
+             </div>
         )
     }
 )
@@ -43,5 +47,5 @@ Input.displayName = "Input"
 
 export {
     Input,
-    // inputVariants
+    inputVariants
 }
