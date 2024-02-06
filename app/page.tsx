5de79fc9cd035e307813a67e6d311067124ecb1a
brunnogrb <brunno@grbtec.com.br>
2024-02-06 09:42:46 -0300

    fix input

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import {Input} from "@/components/reviewing/input"
 
 export default function Page() {
     return (
-        <div className="flex justify-center items-center w-full min-h-screen bg-blue-500">
+        <div className="flex justify-center items-center w-full min-h-screen bg-gray-300">
             <div className="w-1/2 h-1/2 bg-gray-500 rounded-[8px]">
-      <Input subtext={"dasdasd"} subtextClasses={"bg-red-500"} placeholderClasses={"bg-gray-500"} placeholderText="oi"/>
+      <Input subtext={"dasdasd"} subtextClasses={"bg-red-500"} placeholderClasses={"bg-gray-100"} placeholderText="oi"/>
             </div>
         </div>
     )
