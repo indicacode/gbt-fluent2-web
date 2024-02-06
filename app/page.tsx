d5c0a1d17048a922dfa4bc96de448ee666eb4281
Willien <willien_dev@outlook.com>
2024-02-06 09:21:54 -0300

    feat[gbt-fluent2-web]ReAdd input for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/done/avatar"
+import {Input} from "@/components/reviewing/input"
 
 export default function Page() {
     return (
         <div>
-            <Avatar variant="default">
-                <AvatarImage src="https://th.bing.com/th/id/OIP.DGu9im2kr0eKsYb4EkI7ZQHaEo?w=2560&h=1600&rs=1&pid=ImgDetMain"/>
-                <AvatarFallback/>
-            </Avatar>
+      <Input subtext={"dasdasd"} subtextClasses={"bg-red-500"} placeholderClasses={"bg-gray-500"} placeholderText="oi"/>
         </div>
     )
 }
--- components/reviewing/input.tsx
+++ components/reviewing/input.tsx
@@ -1,51 +1,37 @@
 import * as React from "react"
-
-import {cn} from "@/lib/utils"
+import { cn } from "@/lib/utils"
 import {tv} from "tailwind-variants";
 
 const inputVariants = tv(
-  {
-  base:"flex  w-full rounded-md  bg-transparent px-4 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
-    variants: {
-      variant: {
-        default:
-          "flex  w-full   bg-transparent px-2 border-t-2 border-l-2 border-r-2 border-b-2 focus:border-b-black focus:border-b-[1px] hover:border-b-[1px] hover:border-b-black text-sm shadow-sm transition-colors ",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-)
-export interface InputProps
-    extends React.InputHTMLAttributes<HTMLInputElement> {
-    className?: string
-    placeholderText:string
-    placeholderClasses:string
-    subtextClasses:string
-    subtext:string
-}
+    {
+    base:"flex  w-full rounded-md  bg-transparent px-4 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+        variants: {
+            variant: {
+                default:"flex  w-full   bg-transparent px-2 border-t-2 border-l-2 border-r-2 border-b-2 focus:border-b-black focus:border-b-[1px] hover:border-b-[1px] hover:border-b-black text-sm shadow-sm transition-colors "
+            }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({className,placeholderText,placeholderClasses,subtextClasses,subtext, ...props}, ref) => {
-        return (
-            <div className={"flex   flex-col"}>
-              <h2 className={" text-black  py-1" + placeholderClasses}>
-                {placeholderText}
-              </h2>
-            <input
-                className={cn(className + " placeholder-slate-400")}
-                ref={ref}
-                {...props}
-            />
-               <h2 className={"" + subtextClasses}>{subtext}</h2>
-             </div>
-        )
+        },
+        defaultVariants: {
+            variant:"default"
+        }
     }
 )
+
+
+const Input = React.forwardRef(({SubtextClasses, className,PlaceholderClasses, PlaceholderText,Subtext, type,variant, ...props }, ref) => {
+    return (
+        (
+            <div className={"flex   flex-col"}>
+                <h2 className={" text-black  py-1"+PlaceholderClasses}>{PlaceholderText}</h2>
+                <input
+                    type={type}
+                    className={cn(inputVariants({ variant, className }))}
+                    ref={ref}
+                    {...props} />
+                <h2 className={""+ SubtextClasses}>{Subtext}</h2>
+            </div>
+        ))
+})
 Input.displayName = "Input"
 
-export {
-    Input,
-    inputVariants
-}
+export { Input, inputVariants }
