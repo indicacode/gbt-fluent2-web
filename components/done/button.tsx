50d3215aa57ae781fa9d440773677f7857882840
brunnogrb <brunno@grbtec.com.br>
2024-02-08 12:07:08 -0300

    feat{gbt-fluent2-web} push tests and new component display button.tsx , /test/page.tsx , page.tsx tabs.tsx

--- components/done/button.tsx
+++ components/done/button.tsx
@@ -42,14 +42,17 @@ const buttonVariants = tv(
 )
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({className, variant, size, asChild = false, ...props}, ref) => {
+    ({className,hasIcon,Icon, variant, size, asChild = false, ...props}, ref) => {
         const Comp = asChild ? Slot : "button"
         return (
+            <>
+                <i>{ hasIcon?  Icon :  ""}</i>
             <Comp
                 className={buttonVariants({variant, size, className})}
                 ref={ref}
                 {...props}
             />
+            </>
         )
     }
 )
