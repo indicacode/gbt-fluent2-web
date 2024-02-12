dc608af50c97abda5398c1406c0c69c74103ced0
Willien <willien_dev@outlook.com>
2024-02-12 17:56:22 -0300

    feat[gbt-fluent2-web]finish input

--- components/reviewing/button.tsx
+++ components/reviewing/button.tsx
@@ -1,101 +1,113 @@
 import * as React from "react"
-import {Slot} from "@radix-ui/react-slot"
+import { ReactNode } from "react"
+import { ChevronDown } from "lucide-react"
+import { tv, VariantProps } from "tailwind-variants"
+
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuTrigger
-} from "@/components/not-done/dropdown-menu";
-import {tv, VariantProps} from "tailwind-variants";
-import {ChevronDown} from "lucide-react";
-import {ReactNode} from "react";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuTrigger,
+} from "@/components/not-done/dropdown-menu"
 
 export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-        VariantProps<typeof buttonVariants> {
-    Icon?: undefined
-    hasIcon?: boolean
-    asChild?: boolean
-    buttonType?: string
-    dropdownContent?: ReactNode
-    dropdownclassName?: string
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  Icon?: undefined
+  hasIcon?: boolean
+  asChild?: boolean
+  buttonType?: string
+  dropdownContent?: ReactNode
+  dropdownclassName?: string
 }
 
-const buttonVariants = tv(
-    {
-        base: "inline-flex items-center select-none justify-center cursor-pointer whitespace-nowrap rounded-sm text-sm font-medium text-lg transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none text-center disabled:opacity-50 dark:focus-visible:ring-slate-300",
-        variants: {
-            variant: {
-                default:
-                    " text-whitesmoke text-lg  bg-center px-4 py-2 bg-brandColor-primary hover:bg-brandColor-hover active:bg-brandColor-dark active:border-brandColor-secondary active:gray-200 transition-colors ease-out duration-400 ",
+const buttonVariants = tv({
+  base: "inline-flex cursor-pointer select-none items-center justify-center whitespace-nowrap rounded-sm text-center text-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-slate-300",
+  variants: {
+    variant: {
+      default:
+        " text-whitesmoke active:gray-200  duration-400 bg-brandColor-primary bg-center px-4 py-2 text-lg transition-colors ease-out hover:bg-brandColor-hover active:border-brandColor-secondary active:bg-brandColor-dark ",
 
-                outline:
-                    " border-brandColor-primary border-2 text-lg px-4 py-2 active:bg-brandColor-secondary hover:bg-brandColor-secondary transition-colors duration-400 transition-colors ease-out duration-400  active:border-brandColor-secondary shadow-sm text-white  hover:bg-brandColor-default ",
-                secondary:
-                    "px-4 py-2 text-lg bg-gray-200 hover:bg-gray-300 rounded-sm text-brandColor-primary transition-colors ease-out duration-400 active:bg-gray-400 ",
-                disabled: "bg-gray-600 text-lg px-4 py-2 text-gray-400",
-                link: "text-slate-50 text-lg px-4 py-2 transition-colors ease-out duration-400 underline-offset-4 hover:underline dark:text-slate-50",
-                transparent: " bg-none text-lg hover:text-slate-300 ",
-                subtle:
-                    " bg-none text-lg hover:bg-slate-400 active:bg-slate-500 hover:text-slate-100 ",
-            },
-            size: {
-                default: "h-9 px-4 py-2",
-                sm: "h-8 rounded-md px-3 text-xs",
-                lg: "h-10 rounded-md px-8",
-                icon: "h-9 w-9",
-            },
-        },
-        defaultVariants: {
-            variant: "default",
-            size: "default",
-        },
-    }
-)
+      outline:
+        " duration-400 duration-400 hover:bg-brandColor-default border-2 border-brandColor-primary px-4 py-2 text-lg text-white shadow-sm transition-colors transition-colors  ease-out hover:bg-brandColor-secondary active:border-brandColor-secondary  active:bg-brandColor-secondary ",
+      secondary:
+        "duration-400 rounded-sm bg-gray-200 px-4 py-2 text-lg text-brandColor-primary transition-colors ease-out hover:bg-gray-300 active:bg-gray-400 ",
+      disabled: "bg-gray-600 px-4 py-2 text-lg text-gray-400",
+      link: "duration-400 px-4 py-2 text-lg text-slate-50 underline-offset-4 transition-colors ease-out hover:underline dark:text-slate-50",
+      transparent: " bg-none text-lg hover:text-slate-300 ",
+      subtle:
+        " bg-none text-lg hover:bg-slate-400 hover:text-slate-100 active:bg-slate-500 ",
+    },
+    size: {
+      default: "h-9 px-4 py-2",
+      sm: "h-8 rounded-md px-3 text-xs",
+      lg: "h-10 rounded-md px-8",
+      icon: "h-9 w-9",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+})
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({className, variant,Icon,buttonType,dropdownclassName,dropdownContent,hasIcon, size, asChild = false, ...props}, href) => {
-         switch (buttonType) {
-             case "default":
-             return (
-                 <>
-
-                     <i>{hasIcon ? Icon : ""}</i>
-                     <a
-                         className={buttonVariants({variant, size, className})}
-                         href={href}
-                         {...props}
-                     />
-                 </>
-                )
-             case "split":
-                 return (
-
-                     <div className={"flex-row flex"}>
-                         <i>{hasIcon ? Icon : ""}</i>
-                         <a
-                             className={""+ buttonVariants({variant, size, className})}
-                             href={href}
-                             {...props}
-                             />
-                         <div className={"ml-1 flex px-1 py-0 "+ buttonVariants({variant, size,className})}>
-                         <DropdownMenu>
-
-                             <DropdownMenuTrigger className={"select-none focus:outline-0"}><ChevronDown size={18}/></DropdownMenuTrigger>
-                             <DropdownMenuContent className={ `${dropdownclassName}`}>
-                                 <DropdownMenuGroup>
-                                 {dropdownContent}
-                                 </DropdownMenuGroup>
-                             </DropdownMenuContent>
-                         </DropdownMenu>
-                         </div>
-                     </div>
-                 )
-        }
-
+  (
+    {
+      className,
+      variant,
+      Icon,
+      buttonType = "default",
+      dropdownclassName,
+      dropdownContent,
+      hasIcon,
+      size,
+      asChild = false,
+      ...props
+    },
+    href
+  ) => {
+    switch (buttonType) {
+      case "default":
+        return (
+          <>
+            <i>{hasIcon ? Icon : ""}</i>
+            <a
+              className={buttonVariants({ variant, size, className })}
+              href={href}
+              {...props}
+            />
+          </>
+        )
+      case "split":
+        return (
+          <div className={"flex flex-row"}>
+            <i>{hasIcon ? Icon : ""}</i>
+            <a
+              className={"" + buttonVariants({ variant, size, className })}
+              href={href}
+              {...props}
+            />
+            <div
+              className={
+                "ml-1 flex px-1 py-0 " +
+                buttonVariants({ variant, size, className })
+              }
+            >
+              <DropdownMenu>
+                <DropdownMenuTrigger className={"select-none focus:outline-0"}>
+                  <ChevronDown size={18} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className={`${dropdownclassName}`}>
+                  <DropdownMenuGroup>{dropdownContent}</DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        )
     }
+  }
 )
 Button.displayName = "Button"
 
-export {Button, buttonVariants}
+export { Button, buttonVariants }
--- components/reviewing/input.tsx
+++ components/reviewing/input.tsx
@@ -15,18 +15,20 @@ const icons = {
 
 const inputVariants = tv({
   slots: {
-    base: "flex w-full rounded-md border-[1px] bg-white px-4 py-2 text-sm shadow-sm outline-0",
+    root: "flex flex-col",
+    label: "py-1 text-black dark:text-white",
+    base: "flex w-full rounded-md bg-white px-4 py-2 text-sm shadow-sm outline-0",
   },
   variants: {
     variant: {
-      focus: "a",
-      outline: "bg-transparent",
+      focus: "border-b-2 border-gray-500",
+      outline: "border-[1px] bg-transparent",
       filledDark: "dark:bg-dark/90",
       filledLight: "dark:bg-white/90",
     },
     state: {
       neutral: {
-        base: "border-white/80",
+        base: "",
       },
       success: {
         base: "border-green-500/80",
@@ -55,7 +57,7 @@ export interface InputPropsType
   extends React.InputHTMLAttributes<HTMLInputElement>,
     VariantProps<typeof inputVariants> {
   helperText?: string
-  label: string
+  labelText: string
   state?: keyof typeof icons
   iconOnly?: boolean
 }
@@ -64,19 +66,20 @@ const Input = React.forwardRef<HTMLInputElement, InputPropsType>(
   (props, ref) => {
     const {
       className,
-      label,
+      labelText,
       helperText,
       type,
-      variant,
+      variant = "outline",
       state = "neutral",
       iconOnly = false,
       ...rest
     } = props
     const [focus, setFocus] = useState(false)
+    const [active, setActive] = useState(false)
+    const bottomBar = iconOnly || state === "neutral"
 
-    console.log("state", state)
-    console.log("iconOnly:", iconOnly)
-
+    // By using useCallback the function doesn't get recreated on a new
+    // memory reference every time the component re-renders.
     const handleFocus = useCallback(() => {
       setFocus(true)
     }, [])
@@ -84,61 +87,43 @@ const Input = React.forwardRef<HTMLInputElement, InputPropsType>(
     const handleBlur = useCallback(() => {
       setFocus(false)
     }, [])
-    const { base } = inputVariants({
-      state,
-    })
+
+    const handleMouseDown = useCallback(() => {
+      setActive(true)
+    }, [])
+
+    const handleMouseUp = useCallback(() => {
+      setActive(false)
+    }, [])
+    //-------------------------------------------------------------//
+
+    const { base, root, label } = inputVariants()
     return (
-      <div className={"flex flex-col"}>
+      <div className={root()}>
         <div
-          data-state={state}
           className={`
-            relative
-            z-10
-            h-fit
-            overflow-hidden
-            rounded-md
-            transition-all
-            before:absolute
-            before:bottom-0
-            before:left-[50%]
-            before:h-full
-            before:max-h-[0px]
-            before:w-full
-            before:max-w-[0px]
-            before:translate-x-[-50%]
-            before:scale-y-[1]
-            before:bg-[#106CBD]
-            before:text-white
-            before:transition-all
-            before:content-['']
-            hover:before:max-h-[3px]
-            hover:before:max-w-full
-            hover:before:scale-y-[1.0]
-            ${focus && "before:max-h-[3px] before:max-w-full before:scale-y-[1.0]"}
-            `}
+          ${bottomBar && "pressed relative z-10 h-fit overflow-hidden rounded-md transition-all before:absolute before:bottom-0 before:left-[50%] before:h-full before:max-h-[0px] before:w-full before:max-w-[0px] before:translate-x-[-50%] before:scale-y-[1] before:bg-[#106CBD] before:text-white before:transition-all before:content-['']"}
+          ${focus && "before:max-h-[3px] before:max-w-full before:scale-y-[1.0]"}
+          ${active && "before:max-h-[3px] before:max-w-full before:scale-y-[1.0] before:bg-gray-500"}`}
         >
-          <label className={"py-1 text-black dark:text-white"}>
-            {label}
-            <input
-              data-state={state}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-              type={type}
-              className={base({
-                className,
-                iconOnly: iconOnly,
-                variant,
-              })}
-              ref={ref}
-              {...rest}
-            />
-          </label>
+          <label className={label()}>{labelText}</label>
+          <input
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            type={type}
+            className={base({
+              className,
+              iconOnly: iconOnly,
+              state,
+              variant,
+            })}
+            ref={ref}
+            {...rest}
+          />
         </div>
-        <IconComponent
-          iconOnly={iconOnly}
-          state={state}
-          className={"my-auto  mr-0.5"}
-        >
+        <IconComponent iconOnly={iconOnly} state={state}>
           {helperText}
         </IconComponent>
       </div>
@@ -168,7 +153,7 @@ const iconComponent = tv({
     },
     iconOnly: {
       true: {
-        base: "flex items-center text-black/80 dark:text-white/80",
+        base: "flex text-black/80 dark:text-white/80",
       },
     },
   },
