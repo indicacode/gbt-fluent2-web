739a094a7567b839ed66f665a64c26ed2d612dba
brunnogrb <brunno@grbtec.com.br>
2024-02-12 11:47:42 -0300

    feat{gbt-fluent2-web} move toggle.tsx to done, push changes to test-place/bruno/

--- app/test-place/bruno/page.tsx
+++ app/test-place/bruno/page.tsx
@@ -3,12 +3,13 @@ import {AnimatePresence, motion} from "framer-motion";
 import {Button} from "@/components/done/button";
 
 
+
 export default function Page() {
     return (<>
             <div className={"bg-gray-900 overflow-x-hidden flex  w-screen h-screen"}>
                 <AnimatePresence >
 
-                    <Button variant={"default"} buttonType={"split"} dropdownclassName={} dropdownContent={} > SPLIT BUTTON</Button>
+
 
                 </AnimatePresence>
 
