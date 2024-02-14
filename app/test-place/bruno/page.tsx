11a38b0188a1581c943c2bc28af2a341d47b19ec
brunnogrb <brunno@grbtec.com.br>
2024-02-14 11:51:51 -0300

    feat{gbt-fluent2-web} add switch and slider components to ./done

--- app/test-place/bruno/page.tsx
+++ app/test-place/bruno/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import {AnimatePresence, motion} from "framer-motion";
-import {Button} from "@/components/reviewing/button";
+
 
 
 
@@ -8,12 +8,8 @@ import {Button} from "@/components/reviewing/button";
 
 export default function Page() {
     return (<>
-            <div className={"bg-gray-900 overflow-x-hidden flex  w-screen h-screen"}>
+            <div className={"bg-gray-600 overflow-x-hidden flex  w-screen h-screen"}>
                 <AnimatePresence >
-                    <Button className={""}  variant={"default"} >
-
-                    </Button>
-
 
                 </AnimatePresence>
 
