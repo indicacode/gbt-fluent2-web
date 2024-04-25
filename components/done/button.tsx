d5e9f5973baa61fece804b4bd38e6b19630d7423
Willien <willien_dev@outlook.com>
2024-04-25 18:03:46 -0300

    feat(gbt-fluent2-web): fixing some misbehavior  following the demand of #3

--- components/done/button.tsx
+++ components/done/button.tsx
@@ -1,30 +1,41 @@
-import * as React from "react"
-import { ReactNode } from "react"
+import {
+  ButtonHTMLAttributes,
+  forwardRef,
+  ReactNode,
+  Ref,
+  type MouseEvent,
+} from "react"
 import { tv, VariantProps } from "tailwind-variants"
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   icon?: ReactNode
+  isDisabled?: boolean
 }
 
 const buttonVariants = tv({
-  base: "text-md flex cursor-pointer items-center justify-center rounded-[4px] px-3 py-1 font-medium disabled:pointer-events-none disabled:opacity-50",
+  base: "text-md flex cursor-pointer items-center justify-center px-3 py-1 font-medium data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
   variants: {
     variant: {
       default:
-        "dark:text-whitesmoke active:gray-200  duration-400 bg-brand-primary text-white hover:bg-[#115EA3] active:border-brand-secondary active:bg-[#0C3B5E] disabled:bg-[#F0F0F0] disabled:text-black data-[selected=true]:before:bg-white",
+        "dark:text-whitesmoke active:gray-200  duration-400 bg-brand-primary text-white active:border-brand-secondary active:bg-[#0C3B5E] data-[disabled=true]:bg-[#F0F0F0] data-[disabled=true]:text-black data-[selected=true]:before:bg-white data-[disabled=false]:hover:bg-[#115EA3]",
       secondary:
-        "bg-gray-200 text-black before:border-black hover:bg-gray-300 active:bg-gray-400 disabled:bg-[#F0F0F0] data-[selected=true]:before:bg-black",
+        "bg-gray-200 text-black before:border-black active:bg-gray-400 data-[disabled=true]:bg-[#F0F0F0] data-[selected=true]:before:bg-black data-[disabled=false]:hover:bg-gray-300",
       outline:
-        "border-2 border-brand-primary  shadow-sm before:border-black hover:bg-[#F5F5F5] active:border-slate-400 active:before:border-slate-400 data-[selected=true]:before:bg-black dark:text-white dark:before:border-white dark:hover:bg-white/10",
+        "border-2 border-brand-primary  shadow-sm before:border-black active:border-slate-400 active:before:border-slate-400 data-[selected=true]:before:bg-black data-[disabled=false]:hover:bg-[#F5F5F5] dark:text-white dark:before:border-white data-[disabled=false]:dark:hover:bg-white/10",
       transparent:
-        "bg-transparent hover:text-brand-primary hover:before:border-0 hover:before:bg-brand-primary active:before:bg-brand-primary data-[selected=true]:text-brand-primary data-[selected=true]:before:bg-brand-primary dark:data-[selected=true]:before:border-0",
+        "bg-transparent active:before:bg-brand-primary data-[selected=true]:text-brand-primary data-[selected=true]:before:bg-brand-primary data-[disabled=false]:hover:text-brand-primary data-[disabled=false]:hover:before:border-0 data-[disabled=false]:hover:before:bg-brand-primary dark:data-[selected=true]:before:border-0",
       subtle:
-        "bg-transparent before:border-black hover:bg-[#F5F5F5] hover:before:border-0 hover:before:bg-brand-primary active:bg-[#E0E0E0] active:before:bg-brand-primary data-[selected=true]:bg-[#EBEBEB] data-[selected=true]:before:border-0 data-[selected=true]:before:bg-brand-primary dark:text-white  dark:before:border-slate-300 dark:hover:bg-white/10 dark:active:bg-white/20 dark:data-[selected=true]:bg-[#f0f0f011] dark:data-[selected=true]:hover:bg-white/10 dark:data-[selected=true]:active:bg-white/20",
+        "bg-transparent before:border-black active:bg-[#E0E0E0] active:before:bg-brand-primary data-[selected=true]:bg-[#EBEBEB] data-[selected=true]:before:border-0 data-[selected=true]:before:bg-brand-primary data-[disabled=false]:hover:bg-[#F5F5F5] data-[disabled=false]:hover:before:border-0 data-[disabled=false]:hover:before:bg-brand-primary dark:text-white  dark:before:border-slate-300 dark:active:bg-white/20 dark:data-[selected=true]:bg-[#f0f0f011] dark:data-[selected=true]:hover:bg-white/10 data-[disabled=false]:dark:hover:bg-white/10 dark:data-[selected=true]:active:bg-white/20",
     },
     toggle: {
       true: "aspect-square w-fit before:rounded-full before:border-2 before:p-2 before:content-['']",
+    },
+    shape: {
+      rounded: "rounded-[4px]",
+      circular: "rounded-full",
+      square: "rounded-none",
     },
     size: {
       default: "h-9 py-1",
@@ -34,43 +45,57 @@ const buttonVariants = tv({
     },
   },
   defaultVariants: {
+    shape: "rounded",
     variant: "default",
     size: "default",
   },
 })
 
+function handleToggle(
+  event: MouseEvent<HTMLButtonElement>,
+  isDisabled: boolean | undefined
+): void {
+  const { dataset } = event.currentTarget
+  if (!isDisabled && dataset.toggle === "true") {
+    dataset.selected = dataset.selected === "true" ? "false" : "true"
+  }
+}
+
 function Button(
   {
+    isDisabled = false,
+    onClick = () => {},
     toggle = false,
     icon = false,
     className,
     children,
     variant,
+    shape,
     size,
     ...props
   }: ButtonProps,
-  ref: React.Ref<HTMLButtonElement>
+  ref: Ref<HTMLButtonElement>
 ) {
   console.assert(
-    !toggle && !(children === undefined),
+    !(toggle && children !== undefined),
     "You cannot pass children to a toggle button. Children:" + children
   )
-
   return (
     <button
       ref={ref}
-      data-selected={"false"}
+      aria-disabled={isDisabled}
+      data-disabled={isDisabled.toString()}
+      data-selected="false"
       data-toggle={toggle}
-      onClick={({ currentTarget: target }) =>
-        target.dataset.toggle === "true" &&
-        (target.dataset.selected =
-          target.dataset.selected === "false" ? "true" : "false")
-      }
+      onClick={(event) => {
+        handleToggle(event, isDisabled)
+        onClick(event)
+      }}
       className={buttonVariants({
         className,
         variant,
+        shape,
         toggle,
-        size,
       })}
       {...props}
     >
@@ -82,6 +107,6 @@ function Button(
 
 Button.displayName = "Button"
 
-const ForwardedButton = React.forwardRef(Button)
+const ForwardedButton = forwardRef(Button)
 
 export { ForwardedButton as Button }
