bba7e47d930fe81cdcad2e305ae345b384ac9734
brunno <brunno@grbtec.com.br>
2024-06-27 11:57:49 -0300

    feat{gbt-fluent2-web} push messagebar docs, fix issue where page wouldnt load

--- app/page.inputs.tsx
+++ app/page.inputs.tsx
@@ -2251,29 +2251,232 @@ export const components = [
         ),
       },
         {
-            cardHeader: "",
-            cardSubtext: "",
+            cardHeader: "Intent",
+            cardSubtext: "MessageBar components come built-in with preset intents that determine the design and aria live announcement, While it is recommended to use the preset intents, it's possible to configure the aria live politeness with the politeness prop.",
             cardComponent: (
                 <div className="flex flex-col gap-2">
                     <Button
                         variant="outline"
                         onClick={() => {
                             toast({
-                                messageBar: true,
-                                variant: "error",
-                                title: "Scheduled: Catch up ",
-                                description: "Friday, February 10, 2023 at 5:57 PM",
-                                action: (
-                                    <>
-                                        <ToastAction altText="Goto schedule to undo">
-                                            Undo
-                                        </ToastAction>
-                                        <ToastAction altText="Goto schedule to undo">
-                                            Undo
-                                        </ToastAction>
-                                    </>
-                                ),
-                            },
+                                    messageBar: true,
+                                    variant: "error",
+                                    title: "Scheduled: Catch up ",
+                                    description: "Friday, February 10, 2023 at 5:57 PM",
+                                    action: (
+                                        <>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                        </>
+                                    ),
+                                }
+                            )
+                        }}
+                    >
+                        Add to calendar
+                    </Button>
+                </div>
+            ),
+        },
+        {
+            cardHeader: "Shape",
+            cardSubtext: "MessageBar can have either rounded or square corners, please follow the usage guidance for these shapes:\n" +
+                "\n" +
+                "    rounded used for component level message bars\n" +
+                "    square used for page/app level message bars",
+            cardComponent: (
+                <div className="flex flex-col gap-2">
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            toast({
+                                    messageBar: true,
+                                    variant: "error",
+                                    title: "Scheduled: Catch up ",
+                                    description: "Friday, February 10, 2023 at 5:57 PM",
+                                    action: (
+                                        <>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                        </>
+                                    ),
+                                }
+                            )
+                        }}
+                    >
+                        Add to calendar
+                    </Button>
+                </div>
+            ),
+        },
+        {
+            cardHeader: "Actions",
+            cardSubtext: "The MessageBar can have different actions.",
+            cardComponent: (
+                <div className="flex flex-col gap-2">
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            toast({
+                                    messageBar: true,
+                                    variant: "error",
+                                    title: "Scheduled: Catch up ",
+                                    description: "Friday, February 10, 2023 at 5:57 PM",
+                                    action: (
+                                        <>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                        </>
+                                    ),
+                                }
+                            )
+                        }}
+                    >
+                        Add to calendar
+                    </Button>
+                </div>
+            ),
+        },
+        {
+            cardHeader: "Dismiss",
+            cardSubtext: "MessageBar components should be used in a MessageBarGroup when possible to enable exit animations. Once inside a MessageBarGroup component, the default exit animation will trigger automatically when the component is unmounted from DOM.",
+            cardComponent: (
+                <div className="flex flex-col gap-2">
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            toast({
+                                    messageBar: true,
+                                    variant: "error",
+                                    title: "Scheduled: Catch up ",
+                                    description: "Friday, February 10, 2023 at 5:57 PM",
+                                    action: (
+                                        <>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                        </>
+                                    ),
+                                }
+                            )
+                        }}
+                    >
+                        Add to calendar
+                    </Button>
+                </div>
+            ),
+        },
+        {
+            cardHeader: "Animation",
+            cardSubtext: "Enter animations are also handled within the MessageBarGroup. However avoid entry animations for MessageBar components on page load. However, MessageBar components that are mounted during the lifecycle of an app can use enter animations.",
+            cardComponent: (
+                <div className="flex flex-col gap-2">
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            toast({
+                                    messageBar: true,
+                                    variant: "error",
+                                    title: "Scheduled: Catch up ",
+                                    description: "Friday, February 10, 2023 at 5:57 PM",
+                                    action: (
+                                        <>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                        </>
+                                    ),
+                                }
+                            )
+                        }}
+                    >
+                        Add to calendar
+                    </Button>
+                </div>
+            ),
+        },
+        {
+            cardHeader: "Reflow",
+            cardSubtext: "The MessageBar will reflow by default once the body content wraps to a second line. This changes the layout of the actions in the MessageBar.",
+            cardComponent: (
+                <div className="flex flex-col gap-2">
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            toast({
+                                    messageBar: true,
+                                    variant: "error",
+                                    title: "Scheduled: Catch up ",
+                                    description: "Friday, February 10, 2023 at 5:57 PM",
+                                    action: (
+                                        <>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                        </>
+                                    ),
+                                }
+                            )
+                        }}
+                    >
+                        Add to calendar
+                    </Button>
+                </div>
+            ),
+        },
+        {
+            cardHeader: "Manual Layout",
+            cardSubtext: "It's possible to opt out of automatic reflow with the layout prop. This can be useful if an application has an existing responsive design mechanism.",
+            cardComponent: (
+                <div className="flex flex-col gap-2">
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            toast({
+                                    messageBar: true,
+                                    variant: "error",
+                                    title: "Scheduled: Catch up ",
+                                    description: "Friday, February 10, 2023 at 5:57 PM",
+                                    action: (
+                                        <>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                            <ToastAction altText="Goto schedule to undo">
+                                                Undo
+                                            </ToastAction>
+                                        </>
+                                    ),
+                                }
+                            )
+                        }}
+                    >
+                        Add to calendar
+                    </Button>
+                </div>
+            ),
+        },
     ],
   },
 ] as const
