af35c1b1ffe51a9ccdfa424fac8a1ecd00b7e420
brunno <brunno@grbtec.com.br>
2024-11-11 09:24:04 -0300

    try to add tests

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,14 @@ const reactRecommended = require("eslint-plugin-react/configs/recommended")
 
 module.exports = [
   reactRecommended, // This is not a plugin object, but a shareable config object
+  {
+    "overrides": [
+      {
+        "files": ["tests/**/*"],
+        "env": {
+          "jest": true
+        }
+      }
+    ]
+  }
 ]
