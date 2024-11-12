679d65aece90b3e39d83cf58af85c62f050c616f
brunno <brunno@grbtec.com.br>
2024-11-12 11:54:00 -0300

    init accordion tests

--- components/done/accordion.tsx
+++ components/done/accordion.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import React from 'react';
 import { Content, Header, Item, Root, Trigger } from "@radix-ui/react-accordion"
 import { ChevronRightIcon } from "@radix-ui/react-icons"
 import {
