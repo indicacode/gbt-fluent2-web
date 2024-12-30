494652399cc51e7acc56c5445a1e30df90643f0b
Willien <willien_dev@outlook.com>
2024-12-30 18:01:01 -0300

    fix(web): add missing params to components
    (cherry picked from commit 6cac416ca94f7cdaab1fded214a1916964702ca2)

--- components/done/radio-group.tsx
+++ components/done/radio-group.tsx
@@ -13,9 +13,10 @@ function RadioGroup({ className, ...props }) {
   )
 }
 
-function RadioGroupItem({ className, ...props }) {
+function RadioGroupItem({ className, value, ...props }) {
   return (
     <RadioGroupPrimitive.Item
+      value={value}
       className={cn(
         "light:border-slate-200 aspect-square h-4 w-4 rounded-full border border-slate-900 text-slate-900 shadow-sm focus:outline-hidden focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:text-slate-50 dark:focus-visible:ring-slate-300",
         className
--- components/reviewing/tabs.tsx
+++ components/reviewing/tabs.tsx
@@ -16,9 +16,10 @@ function TabsList({ className, ...props }) {
   )
 }
 
-function TabsTrigger({ className, ...props }) {
+function TabsTrigger({ className, value, ...props }) {
   return (
     <TabsPrimitive.Trigger
+      value={value}
       className={cn(
         "inline-flex items-center justify-center px-3 py-1 text-sm font-medium whitespace-nowrap transition-all focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-b-2 data-[state=active]:border-b-slate-950 data-[state=active]:text-slate-950 dark:text-slate-400 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:border-b-2 dark:data-[state=active]:border-b-white dark:data-[state=active]:text-slate-50",
         className
@@ -28,9 +29,10 @@ function TabsTrigger({ className, ...props }) {
   )
 }
 
-function TabsContent({ className, ...props }) {
+function TabsContent({ className, value, ...props }) {
   return (
     <TabsPrimitive.Content
+      value={value}
       className={cn(
         "mt-2 ring-offset-white focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 focus-visible:outline-hidden dark:text-slate-200 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
         className
