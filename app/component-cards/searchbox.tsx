1835cfb54bbc76a8987fc88fd510b999dfc9015c
Gustavo <gustavoborgescr09@gmail.com>
2025-08-13 18:05:22 -0300

    feat(web): searchbox 20%

--- app/component-cards/searchbox.tsx
+++ app/component-cards/searchbox.tsx
@@ -8,7 +8,7 @@ export const searchbox_card = {
     "The SearchBox component allows the users to access information with ease, providing flexibility and the ability to clear and filter the search.",
   cards: [
     {
-      cardComponent: <SearchBox></SearchBox>,
+      cardComponent: <SearchBox onChange={() => console.log("aloo")}></SearchBox>,
     },
     {
       cardHeader: "Appearance",
--- components/not-done/searchbox.tsx
+++ components/not-done/searchbox.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { PiMagnifyingGlassLight } from "react-icons/pi"
+import React, { useState } from "react"
+import { PiMagnifyingGlassLight, PiXLight } from "react-icons/pi"
 import { tv } from "tailwind-variants"
 
 const searchboxVariants = tv({
@@ -7,13 +7,45 @@ const searchboxVariants = tv({
   defaultVariants: {},
 })
 
-type searchboxProps = React.ComponentPropsWithoutRef<"input"> & {}
+type SearchBoxProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  showMode?: "selecting" | "focusAndValue"
+}
 
-export function SearchBox({ children, className, ...props }: searchboxProps) {
+export function SearchBox({
+  value,
+  defaultValue,
+  onChange,
+  showMode = "selecting",
+  ...props
+}: SearchBoxProps) {
+  const [selected, setSelected] = useState<boolean>(false)
   return (
-    <div className="rounded-md border-1 border-gray-500 text-black">
-      <PiMagnifyingGlassLight />
-      <input className={searchboxVariants({})}>{children}</input>
+    <div
+      tabIndex={-1}
+      className="flex items-center justify-between gap-2 rounded-md border border-gray-500 px-3 py-2 text-black"
+    >
+      <div className="flex w-full items-center gap-2">
+        <PiMagnifyingGlassLight />
+        <input
+          defaultValue={defaultValue}
+          value={value}
+          onFocus={() => setSelected(true)}
+          onBlur={(e) => {
+            if (!e.currentTarget.contains(e.relatedTarget)) {
+              setSelected(false)
+            }
+          }}
+          onChange={onChange}
+          type="text"
+          className={searchboxVariants({}) + " w-full outline-0"}
+          {...props}
+        />
+      </div>
+      {selected && (
+        <button className="hover:cursor-pointer" onClick={() => value == ""}>
+          <PiXLight />
+        </button>
+      )}
     </div>
   )
 }
